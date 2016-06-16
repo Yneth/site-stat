21cd74610ad11ae19bd8b3311f35463eb1bb1035
Anthony <zaknofein2@gmail.com>
2016-06-16 13:28:11 +0300

    implemented authproviderservice

--- web/app/services/auth/auth-jwt.service.js
+++ web/app/services/auth/auth-jwt.service.js
@@ -1,14 +1,76 @@
 (function () {
     'use strict';
-    // TODO: implement
 
     angular
         .module('socialStatApp')
         .factory('AuthServerProvider', AuthServerProvider);
 
-    AuthServerProvider.$inject = [];
+    AuthServerProvider.$inject = ['$http', '$q', '$localStorage', '$sessionStorage'];
 
-    function AuthServerProvider() {
-        
+    function AuthServerProvider($http, $q, $localStorage, $sessionStorage) {
+        var service = {
+            getToken: getToken,
+            hasValidToken: hasValidToken,
+            login: login,
+            loginWithToken: loginWithToken,
+            storeAuthenticationToken: storeAuthenticationToken,
+            logout: logout
+        };
+
+        return service;
+
+        function getToken() {
+            return $localStorage.authenticationToken || $sessionStorage.authenticationToken;
+        }
+
+        function hasValidToken() {
+            var token = this.getToken();
+            return token && token.expires && token.expires > new Date().getTime();
+        }
+
+        function login(credentials) {
+            var data = {
+                'username': credentials.username,
+                'password': credentials.password,
+                'rememberMe': credentials.rememberMe
+            };
+
+            return $http.post('api/authenticate', data).success(authenticateSuccess);
+
+            function authenticateSuccess(data, status, headers) {
+                var token = headers('Authentication');
+                if (angular.isDefined(token) && token.slice(0, 7) === 'Bearer ') {
+                    var jwt = token.slice(7, token.length);
+                    service.storeAuthenticationToken(jwt);
+                    return jwt;
+                }
+            }
+        }
+
+        function loginWithToken(jwt, rememberMe) {
+            var deferred = $q.defer();
+
+            if (angular.isDefined(jwt)) {
+                this.storeAuthenticationToken(jwt, rememberMe);
+                deferred.resolve(jwt);
+            } else {
+                deferred.reject();
+            }
+
+            return deferred.promise();
+        }
+
+        function storeAuthenticationToken(jwt, rememberMe) {
+            if (rememberMe) {
+                $localStorage.authenticationToken = jwt;
+            } else {
+                $sessionStorage.authenticationToken = jwt;
+            }
+        }
+
+        function logout() {
+            delete $localStorage.authenticationToken;
+            delete $sessionStorage.authenticationToken;
+        }
     }
 })();
