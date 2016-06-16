af45375577dfeb9b7aff424e7d445f290fc07d6b
Anthony <zaknofein2@gmail.com>
2016-06-16 23:00:18 +0300

    Added method to UserDAO findOneByLoginWIthAuthorities but not sure if it works because join fetch with 'where' clause always returns null check link more info http://stackoverflow.com/questions/22674248/jpa-eclipselink-join-fetch-lazy-relation-returning-null and removed use of getSingleResult cuz it throws RuntimeError

--- src/main/java/ua/social/dao/jpa/UserJpaDAO.java
+++ src/main/java/ua/social/dao/jpa/UserJpaDAO.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Repository;
 import ua.social.dao.UserDAO;
 import ua.social.domain.User;
 
-import javax.persistence.EntityManager;
+import javax.persistence.EntityGraph;
 import javax.persistence.Query;
+import javax.persistence.Subgraph;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -18,6 +25,20 @@ public class UserJpaDAO extends AbstractJpaDAO<User> implements UserDAO {
     public Optional<User> findOneByLogin(String login) {
         Query query = entityManager.createQuery("select u from User u where u.login = :login");
         query.setParameter("login", login);
+
+        // link that describes choice of getResultList over getSingleResult
+        // http://sysout.be/2011/03/09/why-you-should-never-use-getsingleresult-in-jpa/
+        return query.getResultList().stream().findFirst();
+    }
+
+    @Override
+    public Optional<User> findOneByLoginWithAuthorities(String login) {
+//        TODO: check if it works
+//        join fetch with 'where' clause always returns null
+//        http://stackoverflow.com/questions/22674248/jpa-eclipselink-join-fetch-lazy-relation-returning-null
+        Query query = entityManager.createQuery("select u from User u where u.login = :login");
+        query.setParameter("login", login);
+
         return query.getResultList().stream().findFirst();
     }
 }
