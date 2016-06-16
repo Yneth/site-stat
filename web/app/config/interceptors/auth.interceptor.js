96c61727e541fd7c036706da11f950f9c1aa51bf
Anthony <zaknofein2@gmail.com>
2016-06-16 13:27:54 +0300

    implemented auth.interceptor

--- web/app/config/interceptors/auth.interceptor.js
+++ web/app/config/interceptors/auth.interceptor.js
@@ -1,14 +1,27 @@
 (function () {
     'use strict';
-    // TODO: implement
+
     angular
         .module('socialStatApp')
         .factory('authInterceptor', authInterceptor)
 
-    authInterceptor.$inject = [];
+    authInterceptor.$inject = ['$localStorage', '$sessionStorage'];
 
-    function authInterceptor() {
-        var service = {};
+    function authInterceptor($localStorage, $sessionStorage) {
+        var service = {
+            request: request
+        };
         return service;
+
+        function request(config) {
+            config.headers = config.headers || {};
+
+            var token = $localStorage.authenticationToken || $sessionStorage.authenticationToken;
+            if (token) {
+                config.headers.Authorization = 'Bearer ' + token;
+            }
+
+            return config;
+        }
     }
 })();
