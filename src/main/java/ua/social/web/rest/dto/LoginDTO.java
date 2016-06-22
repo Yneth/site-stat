e97fc1bd20e4121fdce34a5ecb4fd64d7d927a7e
Anthony <zaknofein2@gmail.com>
2016-06-22 19:45:34 +0300

    Added validation logic to LoginDTO

--- src/main/java/ua/social/web/rest/dto/LoginDTO.java
+++ src/main/java/ua/social/web/rest/dto/LoginDTO.java
@@ -1,8 +1,21 @@
 package ua.social.web.rest.dto;
 
+import ua.social.config.Constants;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class LoginDTO {
+    @NotNull
+    @Pattern(regexp = Constants.LOGIN_REGEX)
+    @Size(min = 0, max = 50)
     private String username;
+
+    @NotNull
+    @Size(min = ManagedUserDTO.PASSWORD_MIN_LENGTH, max = ManagedUserDTO.PASSWORD_MAX_LENGTH)
     private String password;
+
     private Boolean rememberMe;
 
     public String getUsername() {
@@ -15,5 +28,14 @@ public class LoginDTO {
 
     public Boolean isRememberMe() {
         return rememberMe;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", rememberMe=" + rememberMe +
+                '}';
     }
 }
