39f777345f7ef07775f3fd92f9949a53ca158e96
Anthony <zaknofein2@gmail.com>
2016-06-22 11:57:19 +0300

    Added logging to PermissionEvaluator

--- src/main/java/ua/social/security/acl/impl/PermissionEvaluatorImpl.java
+++ src/main/java/ua/social/security/acl/impl/PermissionEvaluatorImpl.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 import ua.social.security.acl.SecuredDomain;
 import ua.social.security.acl.SecuredDomainService;
 
 import java.io.Serializable;
 
+@Component("permissionEvaluator")
 public class PermissionEvaluatorImpl implements PermissionEvaluator {
     private final Logger log = LoggerFactory.getLogger(PermissionEvaluatorImpl.class);
 
@@ -22,25 +24,36 @@ public class PermissionEvaluatorImpl implements PermissionEvaluator {
 
     @Override
     public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
-        if (authentication == null || !(permission instanceof String)) return false;
+        if (authentication == null || !(permission instanceof String)) {
+            log.debug("Unauthorized access or wrong permission type.");
+            return false;
+        }
 
         String securedDomainName = targetDomainObject.getClass().getSimpleName();
         SecuredDomain domain = securedDomainService.loadDomain(securedDomainName, targetDomainObject);
-        if (domain == null) return false;
+        if (domain == null) {
+            log.debug("Domain is not registered as SecuredDomain Object {}", targetDomainObject);
+            return false;
+        }
 
         Object principal = authentication.getPrincipal();
-        if (principal == null || !(principal instanceof Owner)) return false;
+        if (principal == null || !(principal instanceof Owner)) {
+            log.debug("Principal Object {} does not inherit Owner interface.", principal);
+            return false;
+        }
 
         Owner owner = (Owner) principal;
 
-        if (!owner.getId().equals(domain.getOwnerId())) return false;
+        if (!owner.getId().equals(domain.getOwnerId())) {
+            log.debug("Owner {} does not own a SecuredDomain {}", owner, domain);
+            return false;
+        }
 
         return true;
     }
 
     @Override
     public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
-        // TODO: implement
-        return false;
+        throw new UnsupportedOperationException();
     }
 }
