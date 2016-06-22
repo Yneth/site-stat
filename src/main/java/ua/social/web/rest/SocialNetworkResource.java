99997cafe073f9832f7a9b9739594acb31fabb6b
Anthony <zaknofein2@gmail.com>
2016-06-22 19:45:15 +0300

    Class name refactoring

--- src/main/java/ua/social/web/rest/SocialNetworkResource.java
+++ src/main/java/ua/social/web/rest/SocialNetworkResource.java
@@ -1,22 +1,26 @@
 package ua.social.web.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ua.social.dao.SocialNetworkDAO;
-import ua.social.domain.SocialNetwork;
+import ua.social.web.rest.dto.SocialNetworkDTO;
 
 @RestController
 @RequestMapping("/api")
-public class SocialNetworkController {
+public class SocialNetworkResource {
 
     @Autowired
     private SocialNetworkDAO socialNetworkDAO;
 
     @RequestMapping(value = "/network/{id}", method = RequestMethod.GET)
-    public SocialNetwork getNetworkById(@PathVariable("id") Long id) {
-        return socialNetworkDAO.getById(id).orElseGet(null);
+    public ResponseEntity<SocialNetworkDTO> getNetworkById(@PathVariable("id") Long id) {
+        return socialNetworkDAO.getById(id)
+                .map(sc -> new ResponseEntity<>(new SocialNetworkDTO(sc), HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
