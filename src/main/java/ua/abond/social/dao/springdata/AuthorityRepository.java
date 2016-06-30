460b8b8c5bbcae851091ab250ccfdd9b42cac63a
Anthony <zaknofein2@gmail.com>
2016-06-30 17:30:40 +0300

    Added missing @Param

--- src/main/java/ua/abond/social/dao/springdata/AuthorityRepository.java
+++ src/main/java/ua/abond/social/dao/springdata/AuthorityRepository.java
@@ -2,6 +2,7 @@ package ua.abond.social.dao.springdata;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.abond.social.dao.AuthorityDAO;
 import ua.abond.social.domain.Authority;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
 public interface AuthorityRepository extends AuthorityDAO, JpaRepository<Authority, Long> {
     @Override
     @Query("select a from Authority a where a.name = :name")
-    Optional<Authority> findOneWithName(String name);
+    Optional<Authority> findOneWithName(@Param("name") String name);
 }
