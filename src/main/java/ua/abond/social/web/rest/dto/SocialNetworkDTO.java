cfb42c0db77bfcb79aca66b38f0572fd4fca7576
Anthony <zaknofein2@gmail.com>
2016-06-30 17:33:05 +0300

    Added Id to SocialNetworkDTO

--- src/main/java/ua/abond/social/web/rest/dto/SocialNetworkDTO.java
+++ src/main/java/ua/abond/social/web/rest/dto/SocialNetworkDTO.java
@@ -2,12 +2,16 @@ package ua.abond.social.web.rest.dto;
 
 import org.hibernate.validator.constraints.URL;
 import ua.abond.social.domain.SocialNetwork;
+import ua.abond.social.domain.SocialNetworkSession;
 import ua.abond.social.domain.User;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SocialNetworkDTO {
-
+    @NotNull
+    private Long id;
     @NotNull
     private String name;
     @URL
@@ -16,15 +20,20 @@ public class SocialNetworkDTO {
 
     private UserDTO user;
 
-    public SocialNetworkDTO(String name, String url, User user) {
+    private List<SocialNetworkSession> sessions = new ArrayList<>();
+
+    public SocialNetworkDTO(Long id, String name, String url, User user) {
         setName(name);
         setUrl(url);
         this.user = new UserDTO(user.getLogin(), user.getFirstName(), user.getLastName(),
                 user.getEmail(), user.getActivated(), user.getLangKey(), null);
     }
 
+    public SocialNetworkDTO() {
+    }
+
     public SocialNetworkDTO(SocialNetwork sc) {
-        this(sc.getName(), sc.getUrl(), sc.getUser());
+        this(sc.getId(), sc.getName(), sc.getUrl(), sc.getUser());
     }
 
     public String getName() {
@@ -49,5 +58,21 @@ public class SocialNetworkDTO {
 
     public void setUser(UserDTO user) {
         this.user = user;
+    }
+
+    public List<SocialNetworkSession> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<SocialNetworkSession> sessions) {
+        this.sessions = sessions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
