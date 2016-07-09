35af735463c3893fac47da17d41d6fbbd6931faf
Anthony <zaknofein2@gmail.com>
2016-07-09 14:37:55 +0300

    Renamed remaining resources to controllers

---
+++ src/main/java/ua/abond/social/web/rest/AccountController.java
@@ -0,0 +1,57 @@
+package ua.abond.social.web.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ua.abond.social.security.TokenProvider;
+import ua.abond.social.service.UserService;
+import ua.abond.social.web.rest.dto.ManagedUserDTO;
+import ua.abond.social.web.rest.dto.UserDTO;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class AccountController {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> isAuthenticated() {
+        return userService.getCurrentUser()
+                .map(u -> new ResponseEntity<>(new UserDTO(u), HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> register(@Valid @RequestBody ManagedUserDTO userDTO, HttpServletResponse response) {
+        HttpHeaders textPlainHeaders = new HttpHeaders();
+        textPlainHeaders.setContentType(MediaType.TEXT_PLAIN);
+
+        return userService.getUserByLogin(userDTO.getLogin())
+                .map(u -> new ResponseEntity<>("login already in use", textPlainHeaders, HttpStatus.BAD_REQUEST))
+                .orElseGet(() -> userService.getUserByEmail(userDTO.getEmail())
+                        .map(u -> new ResponseEntity<>("e-mail address already in use", textPlainHeaders, HttpStatus.BAD_REQUEST))
+                        .orElseGet(() -> {
+                            userService.createUser(userDTO);
+                            return new ResponseEntity<>(HttpStatus.CREATED);
+                        })
+                );
+
+
+    }
+}
---
+++ src/test/java/ua/abond/social/web/rest/AccountControllerTest.java
@@ -0,0 +1,13 @@
+package ua.abond.social.web.rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountControllerTest {
+    @Test
+    public void isAuthenticated() throws Exception {
+
+    }
+
+}
