77b050d76de050f6586fb3cff7beb02dbee686f1
Anthony <zaknofein2@gmail.com>
2016-07-09 17:36:44 +0300

    Fixed queries according to a entity renaming and removed unnecessary comments

--- src/main/java/ua/abond/social/dao/springdata/SiteRepository.java
+++ src/main/java/ua/abond/social/dao/springdata/SiteRepository.java
@@ -9,21 +9,10 @@ import ua.abond.social.domain.Site;
 
 public interface SiteRepository extends SiteDAO, JpaRepository<Site, Long> {
     @Override
-    @Query("select sn from Site sn where sn.user.id = ?1")
+    @Query("select s from Site s where s.user.id = ?1")
     Page<Site> findByUserId(Long id, Pageable pageable);
 
     @Override
-    @Query("select sn from Site sn where sn.user.login IN ?1")
+    @Query("select s from Site s where s.user.login IN ?1")
     Page<Site> findByUserLogin(String login, Pageable pageable);
-
-//    @EntityGraph(value = "Site.sessions", type = EntityGraph.EntityGraphType.LOAD)
-//    Optional<Site> findByIdWithSessions(Long id);
-//
-//    @Override
-//    @EntityGraph(value = "Site.sessions", type = EntityGraph.EntityGraphType.LOAD)
-//    Optional<Site> getByIdWithSessions(Long id);
-//
-//    @Override
-//    @Query("select distinct sn from Site sn join fetch sn.socialNetworkSessions where sn.id = (:id)")
-//    Optional<Site> findByIdFullyFetched(@Param("id") Long id);
 }
