47ac3439aecfe93e1de43f6f9b6cdb4750adb537
Anthony <zaknofein2@gmail.com>
2016-07-10 13:55:13 +0300

    Created SiteSessionDTO

---
+++ src/main/java/ua/abond/social/web/rest/dto/SiteSessionDTO.java
@@ -0,0 +1,51 @@
+package ua.abond.social.web.rest.dto;
+
+import ua.abond.social.domain.SiteSession;
+
+import java.time.ZonedDateTime;
+
+public class SiteSessionDTO {
+    private Long id;
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+    private long duration;
+
+    public SiteSessionDTO(SiteSession session) {
+        setId(session.getId());
+        setDuration(session.getDuration());
+        setStartTime(session.getStartDateTime());
+        setEndTime(session.getEndDateTime());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ZonedDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(ZonedDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public ZonedDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(ZonedDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
