7b4e083ad022c6de6b88a1f168338a24d804cdfe
Anthony <zaknofein2@gmail.com>
2016-07-11 12:47:22 +0300

    Implemented sitedto builder

--- src/main/java/ua/abond/social/web/rest/dto/SiteDTO.java
+++ src/main/java/ua/abond/social/web/rest/dto/SiteDTO.java
@@ -8,6 +8,7 @@ import ua.abond.social.domain.User;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SiteDTO {
     @NotNull
@@ -20,13 +21,22 @@ public class SiteDTO {
 
     private UserDTO user;
 
-    private List<SiteSession> sessions = new ArrayList<>();
+    private List<SiteSessionDTO> sessions = new ArrayList<>();
 
     public SiteDTO(Long id, String name, String url, User user) {
+        setId(id);
         setName(name);
         setUrl(url);
         this.user = new UserDTO(user.getLogin(), user.getFirstName(), user.getLastName(),
-                user.getEmail(), user.getActivated(), user.getLangKey(), null);
+                user.getEmail(), false, user.getLangKey(), null);
+    }
+
+    public SiteDTO(Long id, String name, String url, UserDTO user, List<SiteSessionDTO> sessions) {
+        setId(id);
+        setName(name);
+        setUrl(url);
+        setUser(user);
+        setSessions(sessions);
     }
 
     public SiteDTO() {
@@ -60,11 +70,11 @@ public class SiteDTO {
         this.user = user;
     }
 
-    public List<SiteSession> getSessions() {
+    public List<SiteSessionDTO> getSessions() {
         return sessions;
     }
 
-    public void setSessions(List<SiteSession> sessions) {
+    public void setSessions(List<SiteSessionDTO> sessions) {
         this.sessions = sessions;
     }
 
@@ -74,5 +84,61 @@ public class SiteDTO {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public static SiteDTOBuilder from(Site site) {
+        return new SiteDTOBuilder(site);
+    }
+
+    public static class SiteDTOBuilder {
+        private Long id;
+        private String name;
+        private String url;
+        private UserDTO user;
+        private List<SiteSessionDTO> sessions = new ArrayList<>();
+
+        private Site site;
+
+        private SiteDTOBuilder(Site site) { this.site = site;}
+
+        public SiteDTOBuilder withSessions() {
+            this.sessions = site.getSiteSessions()
+                    .stream()
+                    .map(SiteSessionDTO::new)
+                    .collect(Collectors.toList());
+            return this;
+        }
+
+        public SiteDTOBuilder withId() {
+            this.id = site.getId();
+            return this;
+        }
+
+        public SiteDTOBuilder withUrl() {
+            this.url = site.getUrl();
+            return this;
+        }
+
+        public SiteDTOBuilder withName() {
+            this.name = site.getName();
+            return this;
+        }
+
+        public SiteDTOBuilder withUser() {
+            this.user = new UserDTO(site.getUser());
+            return this;
+        }
+
+        public SiteDTO exceptUser() {
+            return withId().withSessions().withUrl().withName().build();
+        }
+
+        public SiteDTO exceptUserAndSessions() {
+            return withId().withUrl().withName().build();
+        }
+
+        public SiteDTO build() {
+            return new SiteDTO(id, name, url, user, sessions);
+        }
     }
 }
