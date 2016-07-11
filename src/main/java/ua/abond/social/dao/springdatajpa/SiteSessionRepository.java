c534470413f28dbc3d7b4a73f09dfb361ee89ffd
Anthony <zaknofein2@gmail.com>
2016-07-11 18:38:44 +0300

    Implemented query for getBySiteIdBetweenDates in SiteSessionRepository

--- src/main/java/ua/abond/social/dao/springdatajpa/SiteSessionRepository.java
+++ src/main/java/ua/abond/social/dao/springdatajpa/SiteSessionRepository.java
@@ -9,12 +9,23 @@ import org.springframework.data.jpa.repository.Query;
 import ua.abond.social.dao.SiteSessionDAO;
 import ua.abond.social.domain.SiteSession;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
 public interface SiteSessionRepository
-        extends SiteSessionDAO, JpaRepository<SiteSession, Long>, JpaSpecificationExecutor<SiteSession>{
+        extends SiteSessionDAO, JpaRepository<SiteSession, Long> {
     @Override
     @Query("select ss from SiteSession ss where ss.site.id = ?1")
     Page<SiteSession> getBySiteId(Long id, Pageable pageable);
 
-//    @Override
-//    Page<SiteSession> getBySiteIdWithSpec(Long siteId, Specification<SiteSession> spec, Pageable pageable);
+    @Override
+    @Query("select ss.id, ss.start, ss.end, ss.duration from SiteSession ss " +
+            "where " +
+            "ss.id = ?1 " +
+            "and " +
+            "ss.start >= ?2 " +
+            "and " +
+            "ss.end < ?3")
+    Page<SiteSession> getBySiteIdBetweenDates(Long siteId, ZonedDateTime start, ZonedDateTime end, Pageable pageable);
 }
