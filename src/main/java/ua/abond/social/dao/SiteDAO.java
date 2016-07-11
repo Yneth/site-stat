7355969c314fbf9c4665c4409a3449438b7545aa
Anthony <zaknofein2@gmail.com>
2016-07-11 18:36:27 +0300

    Removed unnecessary methods from SiteSessionDAO

--- src/main/java/ua/abond/social/dao/SiteDAO.java
+++ src/main/java/ua/abond/social/dao/SiteDAO.java
@@ -4,10 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import ua.abond.social.domain.Site;
 
+import java.time.LocalDate;
+
 public interface SiteDAO extends DAO<Site> {
     Page<Site> findByUserLogin(String login, Pageable pageable);
     Page<Site> findByUserId(Long id, Pageable pageable);
-//    Optional<Site> findByIdWithSessions(Long id);
-//    Optional<Site> getByIdWithSessions(Long id);
-//    Optional<Site> findByIdFullyFetched(Long id);
 }
