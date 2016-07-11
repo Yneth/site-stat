19917739af8d9c6bf4e091fac6ac6132150fc044
Anthony <zaknofein2@gmail.com>
2016-07-11 12:50:26 +0300

    added method deleteById

--- src/main/java/ua/abond/social/dao/DAO.java
+++ src/main/java/ua/abond/social/dao/DAO.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DAO<Entity> {
-    Optional<Entity> getById(long id);
+    Optional<Entity> getById(Long id);
     <S extends Entity> S save(S entity);
     Page<Entity> findAll(Pageable pageable);
     void delete(Entity entity);
+    void deleteById(Long id);
 }
