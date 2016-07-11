a0d9b2f5188d37e8615d37b24510fb50e7b210f5
Anthony <zaknofein2@gmail.com>
2016-07-11 12:58:47 +0300

    Added findAll by spec for siteSessionDAO

--- src/main/java/ua/abond/social/dao/SiteSessionDAO.java
+++ src/main/java/ua/abond/social/dao/SiteSessionDAO.java
@@ -2,8 +2,14 @@ package ua.abond.social.dao;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import ua.abond.social.domain.Site;
 import ua.abond.social.domain.SiteSession;
+
+import java.util.List;
 
 public interface SiteSessionDAO extends DAO<SiteSession> {
     Page<SiteSession> getBySiteId(Long id, Pageable pageable);
+//    Page<SiteSession> getBySiteIdWithSpec(Long siteId, Specification<SiteSession> spec, Pageable pageable);
+    List<SiteSession> findAll(Specification<SiteSession> spec);
 }
--- src/main/java/ua/abond/social/dao/jpa/SiteSessionJpaDAO.java
+++ src/main/java/ua/abond/social/dao/jpa/SiteSessionJpaDAO.java
@@ -2,20 +2,33 @@ package ua.abond.social.dao.jpa;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 import ua.abond.social.dao.SiteSessionDAO;
 import ua.abond.social.domain.SiteSession;
 
-@Repository
-public class SiteSessionJpaDAO
-        extends AbstractJpaDAO<SiteSession>
-        implements SiteSessionDAO {
-    public SiteSessionJpaDAO() {
-        super(SiteSession.class);
-    }
+import java.util.List;
 
-    @Override
-    public Page<SiteSession> getBySiteId(Long id, Pageable pageable) {
-        throw new UnsupportedOperationException();
-    }
-}
+//@Repository
+//public class SiteSessionJpaDAO
+//        extends AbstractJpaDAO<SiteSession>
+//        implements SiteSessionDAO {
+//    public SiteSessionJpaDAO() {
+//        super(SiteSession.class);
+//    }
+//
+//    @Override
+//    public Page<SiteSession> getBySiteId(Long id, Pageable pageable) {
+//        throw new UnsupportedOperationException();
+//    }
+//
+////    @Override
+////    public Page<SiteSession> getBySiteIdWithSpec(Long siteId, Specification<SiteSession> spec, Pageable pageable) {
+////        throw new UnsupportedOperationException();
+////    }
+//
+//    @Override
+//    public List<SiteSession> getBySiteId(Long siteId, Specification<SiteSession> spec) {
+//        throw new UnsupportedOperationException();
+//    }
+//}
