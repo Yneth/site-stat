fdd7b8eb7daee7356b933b2aeb59e4e79cf862b6
Anthony <zaknofein2@gmail.com>
2016-07-11 18:37:19 +0300

    Added getBySiteIdBetweenDates to SiteSessionDAO

--- src/main/java/ua/abond/social/dao/SiteSessionDAO.java
+++ src/main/java/ua/abond/social/dao/SiteSessionDAO.java
@@ -3,13 +3,18 @@ package ua.abond.social.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ua.abond.social.domain.Site;
 import ua.abond.social.domain.SiteSession;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface SiteSessionDAO extends DAO<SiteSession> {
     Page<SiteSession> getBySiteId(Long id, Pageable pageable);
-//    Page<SiteSession> getBySiteIdWithSpec(Long siteId, Specification<SiteSession> spec, Pageable pageable);
-    List<SiteSession> findAll(Specification<SiteSession> spec);
+
+    // TODO: slow
+    Page<SiteSession> findAll(Specification<SiteSession> spec, Pageable pageable);
+
+    Page<SiteSession> getBySiteIdBetweenDates(Long siteId, ZonedDateTime start, ZonedDateTime end, Pageable pageable);
 }
