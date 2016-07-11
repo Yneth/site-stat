a8130d013a00f706819a27249461f1ed7a97f486
Anthony <zaknofein2@gmail.com>
2016-07-11 12:47:00 +0300

    Added constant for datetime format

--- src/main/java/ua/abond/social/config/Constants.java
+++ src/main/java/ua/abond/social/config/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
     public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]+$";
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String DEFAULT_LANG_KEY = "en";
-
-
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER = "Bearer ";
+    public static final String DATETIME_FORMAT = "HH:mm dd/MM/yyyy";
 
     private Constants() { }
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String BEARER = "Bearer ";
 }
