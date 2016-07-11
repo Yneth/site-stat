986f45e97ce8be7ef302130895aff6076d3aec1b
Anthony <zaknofein2@gmail.com>
2016-07-11 18:40:51 +0300

    Move SiteSessionSpecificationsTest

--- src/test/java/ua/abond/social/dao/SiteSessionSpecificationsTest.java
+++ src/test/java/ua/abond/social/dao/SiteSessionSpecificationsTest.java
@@ -1,4 +1,4 @@
-package ua.abond.social.dao.springdatajpa;
+package ua.abond.social.dao;
 
 
 import org.junit.Before;
@@ -9,9 +9,14 @@ import static org.junit.Assert.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import ua.abond.social.dao.springdatajpa.SiteRepository;
+import ua.abond.social.dao.springdatajpa.SiteSessionRepository;
 import ua.abond.social.domain.Site;
 import ua.abond.social.domain.SiteSession;
 import ua.abond.social.domain.specification.SiteSessionSpecifications;
@@ -35,6 +40,7 @@ public class SiteSessionSpecificationsTest {
 
     private Site parent;
     private List<SiteSession> sessions;
+    private List<SiteSession> lastMonthSessions;
 
     @Before
     public void init() {
@@ -53,17 +59,28 @@ public class SiteSessionSpecificationsTest {
             sessionRepository.save(siteSession);
         }
         parent.setSiteSessions(sessions);
+
+        lastMonthSessions = new ArrayList<>();
+        for (int i = 0; i < COUNT_OF_TEST_SESSIONS; i++) {
+            SiteSession siteSession = new SiteSession();
+            siteSession.setStartDateTime(ZonedDateTime.now().minusMonths(1));
+            siteSession.setEndDateTime(ZonedDateTime.now().minusMonths(1));
+            siteSession.setSite(parent);
+            lastMonthSessions.add(siteSession);
+            sessionRepository.save(siteSession);
+        }
     }
 
     @Test
     public void checkCountOfSessions() {
         assertEquals(sessionRepository.findAll(
-                SiteSessionSpecifications.sessionsOfSite(parent)
-        ).size(), COUNT_OF_TEST_SESSIONS);
+                SiteSessionSpecifications.sessionsOfSite(parent),
+                new PageableImpl(0, 10, 0, null)
+        ).getContent().size(), COUNT_OF_TEST_SESSIONS);
     }
 
     @Test
-    public void checkYesterdaysSessions() {
+    public void checkYesterdaySessions() {
         SiteSession siteSession = new SiteSession();
         siteSession.setStartDateTime(ZonedDateTime.now().minusDays(1));
         siteSession.setEndDateTime(ZonedDateTime.now().minusDays(1).plusHours(6));
@@ -71,32 +88,25 @@ public class SiteSessionSpecificationsTest {
         sessionRepository.save(siteSession);
 
 
-        List<SiteSession> list = sessionRepository.findAll(
-                SiteSessionSpecifications.yesterdaysSessions()
+        Page<SiteSession> page = sessionRepository.findAll(
+                SiteSessionSpecifications.yesterdaySessions(),
+                new PageableImpl(0, 20, 0, null)
         );
-        assertEquals(1, list.size());
+        assertEquals(1, page.getContent().size());
 
-        assertEquals(siteSession, list.get(0));
+        assertEquals(siteSession, page.getContent().get(0));
     }
 
     @Test
     public void lastMonthSessions() {
-        List<SiteSession> expected = new ArrayList<>();
-        for (int i = 0; i < COUNT_OF_TEST_SESSIONS; i++) {
-            SiteSession siteSession = new SiteSession();
-            siteSession.setStartDateTime(ZonedDateTime.now().minusMonths(1));
-            siteSession.setEndDateTime(ZonedDateTime.now().minusMonths(1));
-            siteSession.setSite(parent);
-            expected.add(siteSession);
-            sessionRepository.save(siteSession);
-        }
-
-        List<SiteSession> list = sessionRepository.findAll(
-                SiteSessionSpecifications.sessionsLastMonth()
+        Page<SiteSession> page = sessionRepository.findAll(
+                SiteSessionSpecifications.sessionsLastMonth(),
+                new PageableImpl(0, 20, 0, null)
         );
-        assertEquals(COUNT_OF_TEST_SESSIONS, list.size());
 
-        assertEquals(expected, list);
+        assertEquals(COUNT_OF_TEST_SESSIONS, page.getContent().size());
+
+        assertEquals(lastMonthSessions, page.getContent());
     }
 
     @Test
@@ -118,14 +128,69 @@ public class SiteSessionSpecificationsTest {
         ss.setSite(s);
         sessionRepository.save(ss);
 
-        List<SiteSession> list = sessionRepository.findAll(
+        Page<SiteSession> page = sessionRepository.findAll(
                 SiteSessionSpecifications.and(
-                        SiteSessionSpecifications.yesterdaysSessions(),
+                        SiteSessionSpecifications.yesterdaySessions(),
                         SiteSessionSpecifications.sessionsOfSite(parent)
-                )
+                ),
+                new PageableImpl(0, 20, 0, null)
         );
-        assertEquals(1, list.size());
+        assertEquals(1, page.getContent().size());
 
-        assertEquals(siteSession, list.get(0));
+        assertEquals(siteSession, page.getContent().get(0));
+    }
+
+    private static class PageableImpl implements Pageable {
+        private int pageNumber;
+        private int pageSize;
+        private int offset;
+        private Sort sort;
+
+        public PageableImpl(int pageNumber, int pageSize, int offset, Sort sort) {
+            this.pageNumber = pageNumber;
+            this.pageSize = pageSize;
+            this.offset = offset;
+            this.sort = sort;
+        }
+
+        @Override
+        public int getPageNumber() {
+            return pageNumber;
+        }
+
+        @Override
+        public int getPageSize() {
+            return pageSize;
+        }
+
+        @Override
+        public int getOffset() {
+            return offset;
+        }
+
+        @Override
+        public Sort getSort() {
+            return sort;
+        }
+
+        @Override
+        public Pageable next() {
+            return null;
+        }
+
+        @Override
+        public Pageable previousOrFirst() {
+            return null;
+        }
+
+        @Override
+        public Pageable first() {
+            return null;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return false;
+        }
     }
 }
