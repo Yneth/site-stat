f814ae135673ed800aba6d5f5a289d2a89bcd059
Anthony <zaknofein2@gmail.com>
2016-07-13 13:52:20 +0300

    Implemented site session service js

--- web/app/entities/site-session/site-session.service.js
+++ web/app/entities/site-session/site-session.service.js
@@ -1,13 +1,17 @@
 (function () {
     'use strict';
-    
+
     angular
         .module('socialStatApp')
         .factory('SiteSession', SiteSession);
-    
+
     SiteSession.$inject = ['$resource'];
-    
+
     function SiteSession($resource) {
-        return $resource('/api/user/site/:siteId');
+        return $resource('/api/user/site/:siteId/session/:sessionId', {}, {
+            'query': {method: 'GET', isArray: true, params: {siteId: '@siteId'}},
+            'get': {method: 'GET', isArray: false, params: {siteId: '@siteId', sessionId: '@sessionId'}},
+            'delete': {method: 'DELETE', isArray: false, params: {siteId: '@siteId', sessionId: '@sessionId'}}
+        });
     }
 })();
