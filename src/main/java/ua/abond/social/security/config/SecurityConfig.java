e918ebdb8b73c61b9b2ab0df11092664df69132e
Anthony <zaknofein2@gmail.com>
2017-01-22 23:07:51 +0200

    Migrate xml to java config

---
+++ src/main/java/ua/abond/social/security/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package ua.abond.social.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ua.abond.social.security.jwt.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@PropertySource(value = "classpath:jwt.properties", ignoreResourceNotFound = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private AuthenticationEntryPoint entryPoint;
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .portMapper().http(8085).mapsTo(8443).and()
+                .requiresChannel().anyRequest().requiresSecure().and()
+                .sessionManagement().disable()
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic().authenticationEntryPoint(entryPoint).and()
+                .headers().cacheControl().and().xssProtection();
+    }
+
+    @Bean(name = "authenticationManager")
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
