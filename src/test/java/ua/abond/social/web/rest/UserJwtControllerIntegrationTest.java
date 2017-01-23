0c47153f6fc4f984e0ffec88eac36f61c524e24b
Anton Bondarenko <Anton_Bondarenko@epam.com>
2017-01-23 12:09:26 +0200

    Improve UserJwtControllerIntegrationTest

--- src/test/java/ua/abond/social/web/rest/UserJwtControllerIntegrationTest.java
+++ src/test/java/ua/abond/social/web/rest/UserJwtControllerIntegrationTest.java
@@ -1,10 +1,13 @@
 package ua.abond.social.web.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,10 +18,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import ua.abond.social.service.UserService;
+import ua.abond.social.web.rest.dto.LoginDTO;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration({"/spring/mvc-context.xml"})
+@ContextConfiguration({"/spring/dispatcher-servlet.xml"})
 @TestExecutionListeners({
         DependencyInjectionTestExecutionListener.class,
         DirtiesContextTestExecutionListener.class,
@@ -30,6 +38,10 @@ public class UserJwtControllerIntegrationTest {
 
     @Autowired
     private WebApplicationContext context;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Before
     public void setUp() throws Exception {
@@ -37,7 +49,40 @@ public class UserJwtControllerIntegrationTest {
     }
 
     @Test
-    public void testSuccessfulAuthentication() {
+    @DatabaseSetup("/ds/authentication-ds.xml")
+    public void testSuccessfulAuthentication() throws Exception {
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername("admin");
+        loginDTO.setPassword("admin");
+        mockMvc
+                .perform(post("/api/authenticate", loginDTO)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(loginDTO))
+                ).andExpect(status().isOk());
+    }
 
+    @Test
+    public void testNonValidAuthentication() throws Exception {
+        LoginDTO loginDTO = new LoginDTO();
+        mockMvc
+                .perform(post("/api/authenticate", loginDTO)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(loginDTO))
+                ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testNoSuchUserAuthentication() throws Exception {
+        LoginDTO loginDTO = new LoginDTO();
+        loginDTO.setUsername("test");
+        loginDTO.setPassword("test");
+        mockMvc
+                .perform(post("/api/authenticate", loginDTO)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(loginDTO))
+                ).andExpect(status().isUnauthorized());
     }
 }
