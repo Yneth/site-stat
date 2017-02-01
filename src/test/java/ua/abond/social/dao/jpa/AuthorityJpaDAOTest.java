66688c680adc7593efa13e8c4fb486ec27bd9c94
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-01 16:27:11 +0200

    Add more tests for AuthorityJpaDAO

--- src/test/java/ua/abond/social/dao/jpa/AuthorityJpaDAOTest.java
+++ src/test/java/ua/abond/social/dao/jpa/AuthorityJpaDAOTest.java
@@ -10,31 +10,60 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 import ua.abond.social.domain.Authority;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/spring/jpa-db-context.xml"})
 @TestExecutionListeners({
         DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
         TransactionDbUnitTestExecutionListener.class
 })
+@Transactional
 public class AuthorityJpaDAOTest {
+    private static final String ROLE_ADMIN = "ROLE_ADMIN";
+
     @Autowired
     private AuthorityJpaDAO authorityJpaDAO;
 
     @Test
+    public void testSaveAuthorityShouldSaveToTheDatabase() {
+        String role = "ROLE_TEST";
+
+        Authority authority = new Authority();
+        authority.setName(role);
+        authorityJpaDAO.save(authority);
+
+        Authority actual = authorityJpaDAO.findByName(role).orElse(null);
+        assertNotNull(actual);
+        assertThat(actual.getName(), is(authority.getName()));
+    }
+
+    @Test
+    @DatabaseSetup("/ds/authority-ds.xml")
+    public void testUpdateAuthorityShouldUpdateSuccessfully() {
+        final String newRole = "ROLE_TEST";
+
+        Authority authority = new Authority();
+        authority.setId(-1L);
+        authority.setName(newRole);
+        authorityJpaDAO.save(authority);
+
+        assertNull(authorityJpaDAO.findByName(ROLE_ADMIN).orElse(null));
+        assertNotNull(authorityJpaDAO.findByName(newRole).orElse(null));
+    }
+
+    @Test
     @DatabaseSetup("/ds/authority-ds.xml")
     public void testSuccessfulFindByName() {
-        final String adminRole = "ROLE_ADMIN";
-        Optional<Authority> admin = authorityJpaDAO.findByName(adminRole);
+        Optional<Authority> admin = authorityJpaDAO.findByName(ROLE_ADMIN);
         assertTrue(admin.isPresent());
-        assertTrue(adminRole.equals(admin.orElse(null).getName()));
+        assertTrue(ROLE_ADMIN.equals(admin.orElse(null).getName()));
     }
 
     @Test
