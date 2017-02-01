159ac7dbb36db1914bec81657e5b3c2714c06a42
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-01 14:47:29 +0200

    Add dbunit to tests

--- src/test/java/ua/abond/social/dao/jpa/AuthorityJpaDAOTest.java
+++ src/test/java/ua/abond/social/dao/jpa/AuthorityJpaDAOTest.java
@@ -1,10 +1,15 @@
 package ua.abond.social.dao.jpa;
 
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import ua.abond.social.domain.Authority;
 
 import java.util.Optional;
@@ -14,11 +19,17 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/spring/jpa-db-context.xml"})
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionDbUnitTestExecutionListener.class
+})
 public class AuthorityJpaDAOTest {
     @Autowired
     private AuthorityJpaDAO authorityJpaDAO;
 
     @Test
+    @DatabaseSetup("/ds/authority-ds.xml")
     public void testSuccessfulFindByName() {
         final String adminRole = "ROLE_ADMIN";
         Optional<Authority> admin = authorityJpaDAO.findByName(adminRole);
